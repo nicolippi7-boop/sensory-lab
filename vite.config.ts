0b0d33419329800ea1939aa887fe4e68f5ad66be
Nico <nico.lippi7@gmail.com>
2026-01-17 10:59:27 +0100

    miao3

--- vite.config.ts
+++ vite.config.ts
@@ -2,18 +2,15 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, '.', '');
+  const env = loadEnv(mode, process.cwd(), '');
   return {
     plugins: [react()],
     define: {
       'process.env': env,
-      'process.env.API_KEY': JSON.stringify(env.API_KEY)
-    },
-    build: {
-      outDir: 'dist',
-      sourcemap: true
+      'process.env.API_KEY': JSON.stringify(env.API_KEY || env.VITE_API_KEY),
+      'process.env.VITE_SUPABASE_URL': JSON.stringify(env.VITE_SUPABASE_URL),
+      'process.env.VITE_SUPABASE_ANON_KEY': JSON.stringify(env.VITE_SUPABASE_ANON_KEY),
     }
   }
 })
